30d2b4deccfd5adc7a18e1989134d87da4aad82f
nabin <nkbhaie2017@gmail.com>
2022-01-26 21:42:46 -0700

    add bubble sort function

--- dsa-sorting/bubble.js
+++ dsa-sorting/bubble.js
@@ -1,3 +1,13 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    for(let i = 0; i<arr.length; i++){
+        for(let i = 0; i<arr.length; i++){
+            if(arr[i] > arr[j + 1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
 
 module.exports = bubbleSort;

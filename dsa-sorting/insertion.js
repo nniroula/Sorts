a621bb9dc8a399d3b0acae03a4a79f140e00501b
nabin <nkbhaie2017@gmail.com>
2022-01-26 21:48:41 -0700

    add insertion sort

--- dsa-sorting/insertion.js
+++ dsa-sorting/insertion.js
@@ -1,3 +1,13 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for(let i = 0; i< arr.length; i++){
+        let temp = arr[i];
+        j = i - 1;
+        while (j>= 0 && temp < arr[j]){
+            j = j - 1;
+
+        }
+        arr[j+1] = temp;
+    }
+}
 
 module.exports = insertionSort;

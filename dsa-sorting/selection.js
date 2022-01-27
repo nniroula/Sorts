7e639d12e4634ef9eb3229b3b216595a6c613954
nabin <nkbhaie2017@gmail.com>
2022-01-26 22:23:31 -0700

    add selection sort function

--- dsa-sorting/selection.js
+++ dsa-sorting/selection.js
@@ -1,3 +1,15 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    for(let i = 0; i<arr.length; i++){
+        let min_val_index = i;
+        for( let j = 0; j<arr.length; j++){
+            if(arr[min_val_index] > arr[j]){
+                let temp = arr[min_val_index];
+                arr[min_val_index] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        return arr;
+    }
+}
 
 module.exports = selectionSort;
